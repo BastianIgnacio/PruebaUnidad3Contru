b8ab07b236216a082d719e4e1e669d88739a00ca
Bastian <bastian.sepiulvedagarcia@gmail.com>
2020-04-04 22:05:18 -0400

    TDD clase multa
    prueba: pagar una multa
    resultado : multa pagada exitosamente

--- ClubTenis/src/clubtenis/Multa.java
+++ ClubTenis/src/clubtenis/Multa.java
@@ -5,10 +5,73 @@
  */
 package clubtenis;
 
+import java.util.Date;
+
 /**
  *
  * @author macbook
  */
 public class Multa {
+
+    private int idMulta;
+    private String descripcion;
+    private int monto;
+    private Date fecha;
+    private String estado;
+
+    public Multa(int idMulta, String descripcion, int monto, Date fecha, String estado) {
+        this.idMulta = idMulta;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+    
+    public int getIdMulta() {
+        return idMulta;
+    }
+
+    public void setIdMulta(int idMulta) {
+        this.idMulta = idMulta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public void pagar()
+    {
+        this.estado="Pagada";
+    }
+    
+    
     
 }
